59177213b970114647013207524639024e1c3ffd
cpinzonA <113608127+cpinzonA@users.noreply.github.com>
2022-12-14 07:49:14 -0500

    Add files via upload

---
+++ 2022_12_14_120709_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('description')
+             $table->unsignedBigInteger('user_id');
+            foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+             $table->unsignedBigInteger('tweets_id');
+            foreign('tweets_id')->references('id')->on('tweets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
