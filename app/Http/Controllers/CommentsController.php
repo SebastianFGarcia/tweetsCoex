4485411da55147cf6e2ce52ebbd37cea834ac1b5
Sebastian Garcia <sfernandogarcia@uts.edu.co>
2022-12-14 07:30:28 -0500

    Añadiendo controladores y rutas

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,8 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comments;
 
 class CommentsController extends Controller
 {
-    //
+    public function index()
+    {
+        $comments = Comments::all();
+        return response()->json($comments);
+    }
+
+    public function create()
+    {
+        try {
+            $rules = [
+                'user_id' => 'required',
+                'tweet_id' => 'required',
+                'comment' => 'required',
+            ];
+            $messages = [
+                'user_id.required' => 'User ID is required',
+                'tweet_id.required' => 'Tweet ID is required',
+                'comment.required' => 'Comment is required',
+            ];
+            $this->validate(request(), $rules, $messages);
+            $comment = Comments::create(request()->all());
+            return response()->json($comment);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
+
+    public function update($id){
+        try {
+            $rules = [
+                'user_id' => 'required',
+                'tweet_id' => 'required',
+                'comment' => 'required',
+            ];
+            $messages = [
+                'user_id.required' => 'User ID is required',
+                'tweet_id.required' => 'Tweet ID is required',
+                'comment.required' => 'Comment is required',
+            ];
+            $this->validate(request(), $rules, $messages);
+            $comment = Comments::find($id);
+            $comment->update(request()->all());
+            return response()->json($comment);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
+
+    public function delete($id){
+        try {
+            $comment = Comments::find($id);
+            $comment->delete();
+            return response()->json($comment);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
 }
--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,8 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tweet;
 
 class TweetController extends Controller
 {
-    //
+    public function index()
+    {
+        $tweets = Tweet::all();
+        return response()->json($tweets);
+    }
+
+    public function create()
+    {
+        try {
+            $rules = [
+                'user_id' => 'required',
+                'tweet' => 'required',
+            ];
+            $messages = [
+                'user_id.required' => 'User ID is required',
+                'tweet.required' => 'Tweet is required',
+            ];
+            $this->validate(request(), $rules, $messages);
+            $tweet =Tweet::create(request()->all());
+            return response()->json($tweet);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
+
+    public function update($id){
+        try {
+            $rules = [
+                'user_id' => 'required',
+                'tweet' => 'required',
+            ];
+            $messages = [
+                'user_id.required' => 'User ID is required',
+                'tweet.required' => 'Tweet is required',
+            ];
+            $this->validate(request(), $rules, $messages);
+            $tweet = Tweet::find($id);
+            $tweet->update(request()->all());
+            return response()->json($tweet);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
+
+    public function delete($id){
+        try {
+            $tweet = Tweet::find($id);
+            $tweet->delete();
+            return response()->json($tweet);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
 }
